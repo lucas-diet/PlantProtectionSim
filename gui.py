09532cb03c2e15d3bab90a574ee1b8e7dc31f0c4
lucas-diet <lucas.d1@gmx.net>
2024-08-22 16:47:22 +0200

    Input im init fenster valiedieren lassenm + warnmeldung

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 
 import tkinter as tk
+from tkinter import messagebox
 
 from tooltip import Tooltip
 
@@ -9,14 +10,14 @@ class GUI:
         self.window = tk.Tk()
         self.window.title('Initital Configuration')
         
-        screen_width = self.window.winfo_screenwidth()
-        screen_height = self.window.winfo_screenheight()
+        self.screen_width = self.window.winfo_screenwidth()
+        self.screen_height = self.window.winfo_screenheight()
 
         window_width = 300
         window_height = 300
 
-        center_x = int(screen_width / 2 - window_width / 2)
-        center_y = int(screen_height / 2 - window_height / 2) - 200
+        center_x = int(self.screen_width / 2 - window_width / 2)
+        center_y = int(self.screen_height / 2 - window_height / 2) - 200
         
         self.window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
@@ -25,20 +26,27 @@ class GUI:
         numPlant_lab.place(x=10, y=10)
         numherbi_lab.place(x=10, y=50)
 
-        numPlant_in = tk.Entry(width=10)
-        numHerbi_in = tk.Entry( width=10)
-        numPlant_in.config(bg='white', fg='black')
-        numHerbi_in.config(bg='white', fg='black')
-        numPlant_in.place(x=130, y=10)
-        numHerbi_in.place(x=130, y=50)
+        # Validierungsfunktionen Teams
+        valiPlants_in = (self.window.register(self.inputValidation), '%P', '%W', 1, 16)
+        valiHerbi_in = (self.window.register(self.inputValidation), '%P', '%W', 0, 15)
+
+        self.numPlant_in = tk.Entry(width=10, validate='key', validatecommand=valiPlants_in)
+        self.numHerbi_in = tk.Entry( width=10, validate='key', validatecommand=valiHerbi_in)
+        self.numPlant_in.config(bg='white', fg='black')
+        self.numHerbi_in.config(bg='white', fg='black')
+        self.numPlant_in.place(x=130, y=10)
+        self.numHerbi_in.place(x=130, y=50)
 
         girdWidth_lab = tk.Label(text='grid width')
         gridHeight_lab = tk.Label(text='grid height')
         girdWidth_lab.place(x=10, y=100)
         gridHeight_lab.place(x=10, y=140)
 
-        girdWidth_in = tk.Entry(width=10)
-        gridHeight_in = tk.Entry(width=10)
+        # Validierungsfunktionen Grid
+        valiGrid_in = (self.window.register(self.inputValidation), '%P', '%W', 1, 80)
+
+        girdWidth_in = tk.Entry(width=10, validate='key', validatecommand=valiGrid_in)
+        gridHeight_in = tk.Entry(width=10, validate='key', validatecommand=valiGrid_in)
         girdWidth_in.config(bg='white', fg='black')
         gridHeight_in.config(bg='white', fg='black')
         girdWidth_in.place(x=130, y=100)
@@ -60,13 +68,36 @@ class GUI:
         tip_lab4.place(x=240, y=145)
         Tooltip(tip_lab4, 'Enter the grid heightas a number <= 80.')
 
-        start_btn = tk.Button(text='start')
+        start_btn = tk.Button(text='start', command=self.openSimualtor)
         start_btn.place(x=220, y=250)
 
     def mainloop(self):
         self.window.mainloop()
 
- 
+    def openSimualtor(self):
+        self.simWindow = tk.Tk()
+        self.simWindow.title('Simulator')
+        self.simWindow.geometry(f'{self.screen_width-100}x{self.screen_height-100}')
+
+    def inputValidation(self, inp, widgetName, minVal, maxVal):
+        if inp == '':
+            return True
+        
+        if inp.isdigit():
+            num = int(inp)
+            minVal = int(minVal)
+            maxVal = int(maxVal)
+
+            if minVal <= num <= maxVal:
+                return True
+        
+        widget = self.window.nametowidget(widgetName)
+        widget.bell()
+        messagebox.showwarning("Invalid Input", f"Please enter a number between {minVal} and {maxVal}.")
+        widget = self.window.nametowidget(widgetName)
+        self.window.after(0, lambda: widget.delete(0, tk.END))
+        return False
+
 if __name__ == "__main__":
     gui = GUI()
     gui.mainloop()
