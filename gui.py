b481b1e2bac2034ac5a251f0dcaa57440c060e35
lucas-diet <lucas.d1@gmx.net>
2024-08-23 14:49:52 +0200

    Parameter Frame für Teams eingerichtet

--- gui.py
+++ gui.py
@@ -90,34 +90,11 @@ class GUI:
             messagebox.showwarning('Missing Input', 'Please enter a number')
         
         else:
-            self.simWindow = tk.Tk()
-            self.simWindow.title('Simulator')
-            self.simWindow.geometry(f'{self.screen_width-100}x{self.screen_height-100}')
+            self.createSimWindow()
+            self.createTeams_labs('plants')
+            self.createTeams_labs('herbivors')
 
-            # Toolbar-Frame erstellen
-            toolbar = tk.Frame(self.simWindow, bg='white', height=25)
-            toolbar.grid(row=0, column=0, columnspan=3, sticky='we')
-
-            # Erstelle drei Rahmen (links, mitte, rechts)
-            leftFrame = tk.Frame(self.simWindow, bg='green')
-            centerFrame = tk.Frame(self.simWindow)
-            rightFrame = tk.Frame(self.simWindow, bg='red')
-
-            # Rahmen im Gitter anordnen (Zeile 1, da Zeile 0 für Toolbar reserviert ist)
-            leftFrame.grid(row=1, column=0, sticky='nswe')
-            centerFrame.grid(row=1, column=1, sticky='nswe')
-            rightFrame.grid(row=1, column=2, sticky='nswe')
-
-            # Spaltengewichte einstellen, um die Größe der Bereiche zu steuern
-            self.simWindow.grid_columnconfigure(0, weight=1)  # Linker Bereich
-            self.simWindow.grid_columnconfigure(1, weight=3)  # Mittlerer Bereich (größer)
-            self.simWindow.grid_columnconfigure(2, weight=1)  # Rechter Bereich
-
-            # Zeilengewicht einstellen, um die vertikale Dehnung zu ermöglichen
-            self.simWindow.grid_rowconfigure(0, weight=0)  # Toolbar, kein Dehnen
-            self.simWindow.grid_rowconfigure(1, weight=1)  # Hauptinhalt, dehnbar
-                
-
+    
     def inputValidation(self, inp, widgetName, minVal, maxVal):
         if inp == '':
             return True
@@ -136,6 +113,96 @@ class GUI:
         widget = self.window.nametowidget(widgetName)
         self.window.after(0, lambda: widget.delete(0, tk.END))
         return False
+    
+
+    def createSimWindow(self):
+        self.simWindow = tk.Tk()
+        self.simWindow.title('Simulator')
+        self.simWindow.geometry(f'{self.screen_width-100}x{self.screen_height-100}')
+
+        # Toolbar-Frame erstellen
+        toolbar = tk.Frame(self.simWindow, bg='white', height=25)
+        toolbar.grid(row=0, column=0, columnspan=3, sticky='we')
+
+        # Erstelle drei Rahmen (links, mitte, rechts)
+        self.leftFrame = tk.Frame(self.simWindow, bg='green')
+        self.centerFrame = tk.Frame(self.simWindow)
+        self.rightFrame = tk.Frame(self.simWindow, bg='red')
+
+        # Rahmen im Gitter anordnen (Zeile 1, da Zeile 0 für Toolbar reserviert ist)
+        self.leftFrame.grid(row=1, column=0, sticky='nswe')
+        self.centerFrame.grid(row=1, column=1, sticky='nswe')
+        self.rightFrame.grid(row=1, column=2, sticky='nswe')
+
+        # Spaltengewichte einstellen, um die Größe der Bereiche zu steuern
+        self.simWindow.grid_columnconfigure(0, weight=1)  # Linker Bereich
+        self.simWindow.grid_columnconfigure(1, weight=3)  # Mittlerer Bereich (größer)
+        self.simWindow.grid_columnconfigure(2, weight=1)  # Rechter Bereich
+
+        # Zeilengewicht einstellen, um die vertikale Dehnung zu ermöglichen
+        self.simWindow.grid_rowconfigure(0, weight=0)  # Toolbar, kein Dehnen
+        self.simWindow.grid_rowconfigure(1, weight=1)  # Hauptinhalt, dehnbar
+
+        self.createPlantsFrame()
+        self.createHerbivorFrame()
+            
+    def createTeams_labs(self, team):
+        labelList = []
+
+        if team == 'plants':
+            plants = int(self.numPlant_in.get())
+
+            for plantLabel in labelList:
+                plantLabel.destroy()
+            labelList.clear()
+
+            for i in range(plants):
+                newLabel = tk.Label(self.leftFrame, text=f'plant {i+1}', font=('Arial', 18))
+                newLabel.grid(row=i+3, column=0, padx=10, pady=10, sticky='w')
+                labelList.append(newLabel)
+        
+        elif team == 'herbivors':
+            herbivors = int(self.numHerbi_in.get())
+
+            for herbivorLabel in labelList:
+                herbivorLabel.destroy()
+            labelList.clear()
+
+            for i in range(herbivors):
+                newLabel = tk.Label(self.rightFrame, text=f'herbivor {i+1}', font=('Arial', 18))
+                newLabel.grid(row=i+3, column=0, padx=10, pady=10, sticky='w')
+                labelList.append(newLabel)
+
+    def createPlantsFrame(self):
+        plantHeader = tk.Frame(self.leftFrame, bg='blue')
+        plantHeader.grid(row=0, column=0, sticky='nswe')
+
+        self.leftFrame.grid_rowconfigure(0, weight=0)  # plantHeader nicht dehnbar in der Höhe
+        self.leftFrame.grid_rowconfigure(1, weight=0)  # darunterliegende Zeile dehnbar
+        self.leftFrame.grid_columnconfigure(0, weight=1)  # gesamte Breite nutzen
+
+        plantsPara_lab = tk.Label(plantHeader, text='plants parameter', bg='blue', font=('Arial', 25))
+        plantsPara_lab.grid(row=2, column=0, sticky='nswe')
+
+        plantHeader.grid_rowconfigure(0, weight=1)
+        plantHeader.grid_columnconfigure(0, weight=1)
+
+    def createHerbivorFrame(self):
+        # Header im rechten Bereich erstellen
+        herbivoreHeader = tk.Frame(self.rightFrame, bg='purple')
+        herbivoreHeader.grid(row=0, column=0, sticky='nswe')
+
+        self.rightFrame.grid_rowconfigure(0, weight=0)  
+        self.rightFrame.grid_rowconfigure(1, weight=0)  
+        self.rightFrame.grid_columnconfigure(0, weight=1)
+
+        # Text im Header zentrieren
+        herbivorePara_lab = tk.Label(herbivoreHeader, text="Herbivore Header", bg='purple', font=('Arial', 25))
+        herbivorePara_lab.grid(row=0, column=0, sticky='nsew')
+
+        # Konfiguration der Zeilen und Spalten im herbivoreHeader
+        herbivoreHeader.grid_rowconfigure(0, weight=1)
+        herbivoreHeader.grid_columnconfigure(0, weight=1)
 
 if __name__ == "__main__":
     gui = GUI()
